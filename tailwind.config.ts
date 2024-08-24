5a087cf5b2ac1b6c1960959bdf91d8b3f5c1b87e
Mark Shteyn <markshteyn@Marks-MacBook-Pro.local>
2024-08-23 21:57:11 -0400

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,12 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
+  presets: [require("@relume_io/relume-tailwind")],
   plugins: [],
 };
 export default config;
