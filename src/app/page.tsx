db0f97979f8d52c8d120f45e025f4ebda42bbfa5
Mark Shteyn <markshteyn@Marks-MacBook-Pro.local>
2024-08-24 10:53:55 -0400

    select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Button from '@/app/components/elements/button';
 import Input from '@/app/components/elements/input';
+import Select from '@/app/components/elements/select';
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-screen p-32'>
+      <Select placeholder='Select a fruit' options={[
+        { value: 'apple', label: 'Apple' },
+        { value: 'banana', label: 'Banana' },
+        { value: 'cherry', label: 'Cherry' },
+      ]} value='apple' onChange={(value) => console.log(value)} />
     </div>
   );
 }
